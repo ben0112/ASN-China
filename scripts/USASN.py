09f33591b188877495d3980774d98d05db2e2ae7
ben0112 <ben0112@users.noreply.github.com>
2024-10-15 16:41:44 +0800

    Create USASN.py

---
+++ scripts/USASN.py
@@ -0,0 +1,29 @@
+import requests
+from lxml import etree
+import time
+
+def initFile():
+    localTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    with open("ASN.US.list", "w") as asnFile:
+        asnFile.write("// ASN Information in US. (https://github.com/ben0112/ASN-China) \n")
+        asnFile.write("// Last Updated: UTC " + localTime + "\n")
+
+def saveLatestASN():
+    url = "https://bgp.he.net/country/US"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+    }
+    r = requests.get(url = url, headers = headers).text
+    tree = etree.HTML(r)
+    asns = tree.xpath('//*[@id="asns"]/tbody/tr')
+    initFile()
+    for asn in asns:
+        asnNumber = asn.xpath('td[1]/a')[0].text.replace('AS','')
+        asnName = asn.xpath('td[2]')[0].text
+        if asnName != None:
+            asnInfo = "IP-ASN,{}".format(asnNumber)
+            with open("ASN.US.list", "a") as asnFile:
+                asnFile.write(asnInfo)
+                asnFile.write("\n")
+
+saveLatestASN()
